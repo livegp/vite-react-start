60cfd6ac88d3943d161d0e384c7f4c180276f9fe
Oleksandr <live.gp@gmail.com>
2023-08-14 23:19:10 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,36 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import eslintPlugin from '@nabla/vite-plugin-eslint';
+import stylelint from 'vite-plugin-stylelint';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import VitePluginSvgSpritemap from '@spiriit/vite-plugin-svg-spritemap';
+import webfontDownload from 'vite-plugin-webfont-dl';
+import zipPack from 'vite-plugin-zip-pack';
+import autoAlias from 'vite-plugin-auto-alias';
+import imagePresets, { widthPreset } from 'vite-plugin-image-presets';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    eslintPlugin(),
+    stylelint({ fix: true }),
+    ViteMinifyPlugin({}),
+    VitePluginSvgSpritemap('./src/icons/*.svg'),
+    webfontDownload(),
+    zipPack(),
+    autoAlias({ mode: 'sync' }),
+    imagePresets({
+      thumbnail: widthPreset({
+        class: 'img thumb',
+        loading: 'lazy',
+        widths: [48, 96],
+        formats: {
+          webp: { quality: 50 },
+          jpg: { quality: 70 }
+        }
+      })
+    })
+  ],
   base: '/vite-react-start/'
 });
